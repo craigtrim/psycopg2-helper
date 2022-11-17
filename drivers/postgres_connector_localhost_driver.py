11b3aec7afe98ad16f2a1f7b7741e1fb64bb1859
Craig Trim <craigtrim@gmail.com>
2022-11-16 16:30:56 -0800

    Create Connector and Driver

---
+++ drivers/postgres_connector_localhost_driver.py
@@ -0,0 +1,24 @@
+from pprint import pprint
+
+from baseblock import Enforcer
+
+from psycopg2_helper.dmo import PostgresConnector
+
+
+def test_connection_1():
+
+    dmo = PostgresConnector(database_name='postgres', host='localhost')
+    assert dmo.conn
+
+
+def main():
+    from drivers import IntegrationWrapper
+    wrapper = IntegrationWrapper()
+
+    wrapper.call(test_connection_1)
+
+    wrapper.deconstruct_env()
+
+
+if __name__ == "__main__":
+    main()
